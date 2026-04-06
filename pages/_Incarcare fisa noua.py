1a064a986db93b27de9d2857f261326401c4f6ae
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-04-06 13:30:56 +0300

    Implement application info dialog with usage details
    Added information dialog for application usage and updates.

--- pages/_Incarcare fisa noua.py
+++ pages/_Incarcare fisa noua.py
@@ -18,7 +18,13 @@ name, user = require_login("🔐 App Login")
 st.title("Fisa disciplinei")
 st.success(f"Bine ai venit, {name}!")
 @st.dialog("info")
-#from docx import Document
+def info():
+ st.write("Aplicația este pusă la dispoziția dumneavoastră pentru a elimina confuziile și neconcordanțele generate de modificările și noile reglementări ARACIS. Formularul _Fișei disciplinei_ a suferit schimbări de structură, iar această aplicație automatizează procesul de actualizare. Toate informațiile introduse de dumneavoastră sunt transpuse automat în noul șablon oficial.")
+ st.write("Datele referitoare la disciplină (denumire, număr de credite, tip de examinare, număr de ore, codul disciplinei etc.) sunt preluate direct din planurile de învățământ. Au fost introduse două capitole noi: Competențe și Rezultatele învățării. Pentru fiecare specializare, formulările oficiale se regăsesc în prezentarea planului de învățământ și sunt afișate de aplicație în etapa de completare. Acestea trebuie adaptate pentru fiecare disciplină în secțiunile corespunzătoare ale fișei.")
+ st.write("Pentru fișele încărcate în anii anteriori în baza de date, aceste capitole conțin propuneri destinate titularului de curs. Disciplinele aflate la prima completare pot fi încărcate în format .docx, iar aplicația va prelua automat cât mai multe dintre informațiile existente. De asemenea, este posibilă completarea manuală prin câmpurile afișate. Propunerile generate pot fi acceptate, preluate sau editate.")
+ st.write("Datele privind aprobarea în departament și consiliu vor fi actualizate automat atunci când devin disponibile. La final, fișa completată poate fi descărcată și vizualizată în formatul oficial aprobat.")
+if st.button("Informatii aplicatie"):
+	info()
 
 def preprocess(text):
     return text.strip().lower().translate(str.maketrans('', '', string.punctuation))
