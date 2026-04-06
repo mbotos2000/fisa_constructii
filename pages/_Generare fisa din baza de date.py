8b499bd692e0cbccfcedd4b3b9d4a26f1f6af68a
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-04-06 14:34:58 +0300

    Update _Generare fisa din baza de date.py

--- pages/_Generare fisa din baza de date.py
+++ pages/_Generare fisa din baza de date.py
@@ -563,7 +563,7 @@ def load_ftp_file():
 
     # Download CSV files
     csv_data = {}
-    for filename in ["lista_cd.csv", "planinv_2025.csv"]:
+    for filename in ["lista_cd.csv", "planinv_2025_v2.csv"]:
         with BytesIO() as file_data:
             ftp_server.retrbinary(f"RETR {filename}", file_data.write)
             file_data.seek(0)  # Reset file pointer to the start
