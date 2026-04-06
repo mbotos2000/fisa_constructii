b93e18fb15d072412c33844523fec0c8b896801d
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-04-06 14:11:01 +0300

    Update _Generare fisa din baza de date.py

--- pages/_Generare fisa din baza de date.py
+++ pages/_Generare fisa din baza de date.py
@@ -924,9 +924,12 @@ if not(st.session_state['ut']):
                st.text_area('4.2 Preconditii de competente',value=data_fis['M_4_2'],key='M_4_2',placeholder="Completati manual. Aplicatia nu a reusit sa identifice text in fisa incarcata!")
                st.text_area('5.1 Conditii de desfasurare a cursului',value=data_fis['M_5_1'],key='M_5_1',placeholder="Completati manual. Aplicatia nu a reusit sa identifice text in fisa incarcata!")        
                st.text_area('5.2 Conditii de desfasurare a aplicatiilor',value=data_fis['M_5_2'],key='M_5_2',placeholder="Completati manual. Aplicatia nu a reusit sa identifice text in fisa incarcata!")
-               with st.expander("Arată prezentarea planului de invatamant pentru a completa urmatoarea sectiune in concordanta cu principiile definite in misiunea specializarii pentru care scriu aceasta fisa"):
+               try:
+                with st.expander("Arată prezentarea planului de invatamant pentru a completa urmatoarea sectiune in concordanta cu principiile definite in misiunea specializarii pentru care scriu aceasta fisa"):
                   pdf_stream=(load_ftp_pdf_file(pres[st.session_state['M_1_6']]))
                   st.pdf(pdf_stream)
+               except:
+				   pass
                add_selectbox_cp = st.multiselect('6.a Competente profesionale acumulate',c_spec,help='Pot fi alese oricate competente.')
 			                           
                ss=''
