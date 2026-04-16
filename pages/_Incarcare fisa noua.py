83633f46ad522d144cb15d62889c714171877777
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-04-16 12:58:07 +0300

    Update _Incarcare fisa noua.py

--- pages/_Incarcare fisa noua.py
+++ pages/_Incarcare fisa noua.py
@@ -857,19 +857,19 @@ if st.session_state['file']!=None or st.session_state['ut']:
     sd=int(tosi-int(slide_37a)-int(slide_37b)-int(slide_37c)-int(slide_37d)-int(slide_37e)-int(slide_37f))
    except:
        pass
-    if not(sd<=0):
+   if not(sd<=0):
         slide_37f=st.slider(
           '(f) Alte activități:',
            max_value=int(tosi-int(slide_37a)-int(slide_37b)-int(slide_37c)-int(slide_37d)-int(slide_37e)-int(slide_37f)),
       
           value=int(tosi-int(slide_37a)-int(slide_37b)-int(slide_37c)-int(slide_37d)-int(slide_37e)-int(slide_37f)),
           help='Completati de la a spre f. Suma orelor de studiu individual este cea din planurile de invatamant')
-    else:
+   else:
             st.write('(f) Alte activități: 0 ore')
             slide_37f=0
             slide_37e+=-sd
-    a=st.button('Treci la capitolul 4')
-    if a:
+   a=st.button('Treci la capitolul 4')
+   if a:
       st.write('Capitolul 4')
       schimba_M_3_7_a(slide_37a)
       schimba_M_3_7_b(slide_37b)
