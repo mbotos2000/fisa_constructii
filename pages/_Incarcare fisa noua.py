f7bf3bd76b6e90961f4ca61583b7a99ecbb34b82
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-06-08 11:48:45 +0300

    Update _Incarcare fisa noua.py

--- pages/_Incarcare fisa noua.py
+++ pages/_Incarcare fisa noua.py
@@ -1282,14 +1282,16 @@ if st.session_state['file']!=None or st.session_state['ut']:
 
         #st.write(st.session_state)
         if (st.session_state['test_curs'])&(st.session_state['test_aplicatie']):
-            if st.session_state['M_1_6']!='Constructii civile, industriale si agricole (CCIA-eng)' or st.session_state['M_1_6']!='Inteligenta artificiala in ing.civila si manag.constructiilor (AICIV-e) ':
+            if st.session_state['M_1_4']!='Inginerie_civila':
+			#st.session_state['M_1_6']!='Constructii civile, industriale si agricole (CCIA-eng)' or st.session_state['M_1_6']!='Inteligenta artificiala in ing.civila si manag.constructiilor (AICIV-e) ':
                 
                 _,_,template,_,_,_,_,_,_=load_ftp_file()
             else:
                 _,_,_,template,_,_,_,_,_=load_ftp_file()
         
         if (st.session_state['test_curs'])&(not(st.session_state['test_aplicatie'])):
-            if st.session_state['M_1_6']!='Constructii civile, industriale si agricole (CCIA-eng)' or st.session_state['M_1_6']!='Inteligenta artificiala in ing.civila si manag.constructiilor (AICIV-e) ':
+            if st.session_state['M_1_4']!='Inginerie_civila':
+			#st.session_state['M_1_6']!='Constructii civile, industriale si agricole (CCIA-eng)' or st.session_state['M_1_6']!='Inteligenta artificiala in ing.civila si manag.constructiilor (AICIV-e) ':
                
                 _,_,_,_,template,_,_,_,_=load_ftp_file()
                 st.session_state['M_1_3']=departamentele[st.session_state['M_1_3']]
@@ -1297,7 +1299,8 @@ if st.session_state['file']!=None or st.session_state['ut']:
                 _,_,_,_,_,template,_,_,_=load_ftp_file()
                 st.session_state['M_1_3']=departamentele[st.session_state['M_1_3']]
         if (not(st.session_state['test_curs']))&(st.session_state['test_aplicatie']):
-            if st.session_state['M_1_6']!='Constructii civile, industriale si agricole (CCIA-eng)' or st.session_state['M_1_6']!='Inteligenta artificiala in ing.civila si manag.constructiilor (AICIV-e) ':
+            if st.session_state['M_1_4']!='Inginerie_civila':
+			#st.session_state['M_1_6']!='Constructii civile, industriale si agricole (CCIA-eng)' or st.session_state['M_1_6']!='Inteligenta artificiala in ing.civila si manag.constructiilor (AICIV-e) ':
                 _,_,_,_,_,_,template,_,_=load_ftp_file()
             else:
                 _,_,_,_,_,_,_,template,_=load_ftp_file()
