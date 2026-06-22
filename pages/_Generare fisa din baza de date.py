8c118d285624ee97ec4967ce26f951b3de0c0aa5
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-06-22 13:11:17 +0300

    Update _Generare fisa din baza de date.py

--- pages/_Generare fisa din baza de date.py
+++ pages/_Generare fisa din baza de date.py
@@ -1030,21 +1030,31 @@ if not(st.session_state['ut']):
                                                 _,_,template,_,_,_,_,_,_,_=load_ftp_file()
                     else:
                         _,_,_,template,_,_,_,_,_,_=load_ftp_file()
-                        st.session_state['M_1_3']=departamentele[data_fis['M_1_3']]
+						try:
+                         st.session_state['M_1_3']=departamentele[data_fis['M_1_3']]
+						except:
+                         st.session_state['M_1_3']=data_fis['M_1_3']
+
                 if bool(st.session_state['test_curs'])& (not(bool(st.session_state['test_aplicatie']))):
                     if st.session_state['M_1_4']!='Inginerie_civila':
 					#st.session_state['M_1_6']!='Constructii civile, industriale si agricole (CCIA-eng)' or st.session_state['M_1_6']!='Inteligenta artificiala in ing.civila si manag.constructiilor (AICIV-e) ':
                                                _,_,_,_,template,_,_,_,_,_=load_ftp_file()
                     else:
                         _,_,_,_,_,template,_,_,_,_=load_ftp_file()
-                        st.session_state['M_1_3']=departamentele[data_fis['M_1_3']]
+						try:
+                         st.session_state['M_1_3']=departamentele[data_fis['M_1_3']]
+						except:
+                         st.session_state['M_1_3']=data_fis['M_1_3']
                 if (not(bool(st.session_state['test_curs'])))& bool(st.session_state['test_aplicatie']):
                     if st.session_state['M_1_4']!='Inginerie_civila':
 					#st.session_state['M_1_6']!='Constructii civile, industriale si agricole (CCIA-eng)' or st.session_state['M_1_6']!='Inteligenta artificiala in ing.civila si manag.constructiilor (AICIV-e) ':
                         _,_,_,_,_,_,template,_,_,_=load_ftp_file()
                     else:
                         _,_,_,_,_,_,_,template,_,_=load_ftp_file()
-                        st.session_state['M_1_3']=departamentele[data_fis['M_1_3']]
+						try:
+                         st.session_state['M_1_3']=departamentele[data_fis['M_1_3']]
+						except:
+                         st.session_state['M_1_3']=data_fis['M_1_3']
                 st.session_state['M_1_3']=data_fis['M_1_3']
                 st.session_state['M_1_2']=data_fis['M_1_2']
                 document = MailMerge(template)
